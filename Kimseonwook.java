93d446ca56637cca45a475b8e5fef65fa112f227
seonwookkim <21700107@handong.edu>
2020-05-20 01:21:09 +0900

    20.05.20

---
+++ Kimseonwook.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Kimseonwook
+{
+	public static void main (String [] args)
+	{
+		int n = 21700107;
+		String name = "Kim Seon Wook.";
+		String univ = "Handong Global University." ;
+		
+		System.out.println("Name : " + name);
+		System.out.println("University : " + univ);
+		System.out.println("Student Number : " +n);
+	}
+}
