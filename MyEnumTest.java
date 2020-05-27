a61746097b300b0d107a82a5fb85c21224765523
seonwookkim <21700107@handong.edu>
2020-05-27 13:52:10 +0900

    20.05.27

---
+++ MyEnumTest.java
@@ -0,0 +1,33 @@
+public class MyEunmTest {
+	enum Weeks {MON, TUE, WED, THR, FRI, SAT, SUN}
+	
+	public static void main(String[]args) {
+		Weeks[]arr = Weeks.values();
+		for(Weeks weeks : arr) {
+			switch(weeks) {
+			case MON :
+				System.out.println("Monday!") ;
+				break ;
+			case TUE :
+				System.out.println("Tuesday!") ;
+				break ;
+			case WED :
+				System.out.println("Wedneday!") ;
+				break ;
+			case THR :
+				System.out.println("Thursday!") ;
+				break ;
+			case FRI :
+				System.out.println("Friday!") ;
+				break ;
+			case SAT :
+				System.out.println("Saturday!") ;
+				break ;
+			case SUN :
+				System.out.println("Sunday!") ;
+				break ;
+			}
+		}
+		System.exit(0);
+	}
+}
